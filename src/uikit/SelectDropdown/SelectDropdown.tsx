94ff027acbdccdeeade5b140e30ace4fdcf3d8da
Anthony Lasserre <anthony@getluko.com>
2022-10-06 02:23:18 +0200

    Fix ESLint

--- src/uikit/SelectDropdown/SelectDropdown.tsx
+++ src/uikit/SelectDropdown/SelectDropdown.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-script-url */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import { Fragment } from "react";
 import { Menu, Transition } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
